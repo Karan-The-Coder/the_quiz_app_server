b11f3bb9ff05b657b3b99a76d9cb1e62a7b1d05a
Karan-The-Coder <117984368+Karan-The-Coder@users.noreply.github.com>
2023-01-30 16:06:05 +0530

    Create data.js

---
+++ database/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 1,
+        question: "Javascript is an ________ language",
+        options: [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural'
+        ]
+    },
+    {
+        id: 2,
+        question: "Which one of the following also known as Conditional Expression:",
+        options: [
+            'Switch Statement',
+            'immediate if',
+            'if-then-else statement'
+        ]
+    },
+    {
+        id: 3,
+        question: "If you have a function f and an object o, you can define a method named m of o with _______",
+        options: [
+            `o.m=f`,
+            `o.m=m.f`,
+            `0=f.m`
+        ]
+    },
+    {
+        id: 4,
+        question: ' The "function" and " var" are known as:',
+        options: [
+            'Keywords',
+            'Declaration Statements',
+            'Prototypes'
+        ]
+    },
+    {
+        id: 5,
+        question: "Which of the following variables takes precedence over the others if the names are the same?",
+        options: [
+            'Global variable',
+            'The Local variable',
+            'None of the above'
+        ]
+    },
+]
+
+export const answers = [0,1,0,1,1]; 
